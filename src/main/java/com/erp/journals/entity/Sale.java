78aabd6861827bbf96083e85700012d6943c9ed5
Nikhil <mnikhil1901@gmail.com>
2023-09-14 21:54:35 +0530

    changes made

--- src/main/java/com/erp/journals/entity/Sale.java
+++ src/main/java/com/erp/journals/entity/Sale.java
@@ -1,29 +1,19 @@
 package com.erp.journals.entity;
 
-//import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
-//import com.vladmihalcea.hibernate.type.json.JsonStringType;
-//import org.hibernate.annotations.Type;
-//import org.hibernate.annotations.TypeDef;
-//import org.hibernate.annotations.TypeDefs;
-
 import javax.persistence.*;
-import jdk.jfr.DataAmount;
 import lombok.Data;
 
-import java.math.BigDecimal;
-import java.util.Date;
+import java.io.Serializable;
+import java.sql.Date;
+import java.time.ZonedDateTime;
 
 /** The persistent class for the sales database table. */
 @Data
 @Entity
 @Table(name = "sales")
 @NamedQuery(name = "Sale.findAll", query = "SELECT s FROM Sale s")
-//@TypeDefs({
-//        @TypeDef(name = "json", typeClass = JsonStringType.class),
-//        @TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
-//})
-public class Sale  {
-//    private static final long serialVersionUID = 1L;
+public class Sale  implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @Basic(optional = false)
@@ -31,13 +21,13 @@ public class Sale  {
     private int id;
 
     @Column(name = "invoice_date")
-    private Date invoiceDate;
+    private ZonedDateTime invoiceDate;
 
     @Column(name = "paid_amt")
-    private BigDecimal paidAmount;
+    private Integer paidAmount;
 
-    // bi-directional many-to-one association to Customer
-    @ManyToOne(optional = true, fetch = FetchType.EAGER)
+    // bidirectional many-to-one association to Customer
+    @ManyToOne(fetch = FetchType.EAGER)
     private Customer customer;
 
 }
--- src/main/java/com/erp/journals/repository/SaleRepository.java
+++ src/main/java/com/erp/journals/repository/SaleRepository.java
@@ -4,14 +4,14 @@ import com.erp.journals.entity.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.sql.Date;
-import java.time.Month;
-import java.time.Year;
 import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale,Integer> {
     @Query(value = "FROM Sale WHERE EXTRACT(YEAR FROM invoiceDate) = :year AND EXTRACT(MONTH FROM invoiceDate) = :month")
     List<Sale> findSalesByInvoiceDate(int year, int month);
+
+    @Query(value = "FROM Sale WHERE EXTRACT (YEAR FROM invoiceDate) = :year")
+    List<Sale> findSalesByInvoiceDate(int year);
 
 
 }
