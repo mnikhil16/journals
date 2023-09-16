e7b544e20a65040209069695a377e60ab85850d2
Nikhil <mnikhil1901@gmail.com>
2023-09-16 11:56:26 +0530

    changes made created Journal Entries for the Financial Year 2022-2023

--- src/main/java/com/erp/journals/repository/SaleRepository.java
+++ src/main/java/com/erp/journals/repository/SaleRepository.java
@@ -3,9 +3,11 @@ package com.erp.journals.repository;
 import com.erp.journals.entity.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface SaleRepository extends JpaRepository<Sale,Integer> {
     @Query(value = "FROM Sale WHERE EXTRACT(YEAR FROM invoiceDate) = :year AND EXTRACT(MONTH FROM invoiceDate) = :month")
     List<Sale> findSalesByInvoiceDate(int year, int month);
